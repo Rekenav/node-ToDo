552df531fe907bc4a94e735fb2bd77da2b8f63b2
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-07-26 19:15:38 +0200

    A few changes and refactor work

---
+++ server/voorbeeld.js
@@ -0,0 +1,26 @@
+// var nextTodo = new Todo({
+//     text: 'Do something new',
+//     completed: true,
+//     completedAt: 1559
+// });
+
+// nextTodo.save().then((doc) => {
+//     console.log('saved todo', doc);
+// }, (e) => {
+//     console.log('unable to save todo');
+// });
+
+// new user model
+// email - require and trim - set type to string - set minlength 1
+
+
+
+// var newUser = new Users({
+//     email: "jaap.in.de.boks@gmail.com"
+// });
+
+// newUser.save().then((doc) => {
+//     console.log('Saved new user', doc);
+// }, (e) => {
+//     console.log('unable to save user', e);
+// });
