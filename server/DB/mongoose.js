1b1fb88f5c803ace976376e3884df0eef8616e72
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-08-23 11:17:25 +0200

    changed

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-    mlab : 'mongodb://Edd555:Welkom01@ds155473.mlab.com:55473/todo',
-    localhost : 'mongodb://localhost:27017/TodoApp'
-};
+// let db = {
+//     mlab : 'mongodb://Edd555:Welkom01@ds155473.mlab.com:55473/todo',
+//     localhost : 'mongodb://localhost:27017/TodoApp'
+// };
 
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
 // process.env.PORT ? 
 module.exports = {mongoose};
