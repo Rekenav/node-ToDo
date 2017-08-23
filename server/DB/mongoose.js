6dadddfc37ae76cb0193d2ad12ec85eadc19a4b8
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-08-23 11:02:46 +0200

    Setup app her

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+let db = {
+    mlab : 'mongodb://Edd555:Welkom01@ds155473.mlab.com:55473/todo',
+    localhost : 'mongodb://localhost:27017/TodoApp'
+};
+
+mongoose.connect(db.localhost || db.mlab);
+// process.env.PORT ? 
 module.exports = {mongoose};
