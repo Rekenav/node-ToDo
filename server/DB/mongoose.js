2b341606f8c892702e9df1aafb8c99e87e7bce81
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-08-23 11:45:24 +0200

    fix?

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// let db = {
-//     mlab : 'mongodb://Edd555:Welkom01@ds155473.mlab.com:55473/todo',
-//     localhost : 'mongodb://localhost:27017/TodoApp'
-// };
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
-// process.env.PORT ? 
 module.exports = {mongoose};
