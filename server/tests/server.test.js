7ad1b99c4188ae73198b94ec1bb6b6a22c4bc6b4
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-08-24 15:13:59 +0200

    test the delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,9 +95,47 @@ describe('GET /todos/:id', () => {
     });
 
     it('should return 404 for none object ids', (done) => {
-        //  /todos/123
         request(app)
             .get(`/todos/123abc`)
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return 404 if todo nog found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is not vallid', (done) => {
+        request(app)
+            .delete(`/todos/123abc`)
             .expect(404)
             .end(done);
     });
