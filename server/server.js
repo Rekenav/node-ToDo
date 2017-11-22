2e8f280b9c8fd147405a608ffc81ee77d4fb9837
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-11-22 15:14:54 +0100

    ad POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./DB/mongoose.js');
 var {Todo} = require('./models/todo.js');
-var {Users} = require('./models/user.js');
+var {User} = require('./models/user.js');
 
 var app = express();
 const port = process.env.PORT;
@@ -95,6 +95,21 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+// POST /USERS
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+
 });
 
 app.listen(port, () => {
