01d98041c8b6f73180e4d13d0ccab69c6187ab5a
Eduard Vaneker <eduard.vaneker@gmail.com>
2017-11-29 17:35:32 +0100

    Add test

--- server/server.js
+++ server/server.js
@@ -98,7 +98,6 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
-// POST /USERS
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -118,7 +117,6 @@ app.get('/users/me',authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
